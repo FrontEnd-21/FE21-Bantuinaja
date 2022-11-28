89a04c58a3411fddd4019bef0edd9b5768e6bed6
Sabrinaaafz <fauziasabrina0@gmail.com>
2022-11-28 16:18:52 +0700

    memperbaiki tampilan page lengkapi dokumen dan status bantuan

--- src/Pages/StatusBantuan.jsx
+++ src/Pages/StatusBantuan.jsx
@@ -1,75 +1,104 @@
-import React from 'react'
-import '../assets/CSS/StatusBantuan.css'
-import nus from '../assets/IMAGE/NUS.svg'
-import ld from '../assets/IMAGE/Rectangles.svg'
-import ds from '../assets/IMAGE/description.svg'
-import lx from '../assets/IMAGE/vecto.svg'
-import dc from '../assets/IMAGE/check_circle_outline.svg'
+import React from "react";
 
+// CSS
+import "../assets/CSS/StatusBantuan.css";
 
+// Image
+import nus from "../assets/IMAGE/NUS.svg";
+
+// Bootstrap
+import Card from "react-bootstrap/Card";
+
+// Router
+import { Link } from "react-router-dom";
+
+// Icons
+import { FaChevronLeft } from "react-icons/fa";
+import { FaRegFileAlt } from "react-icons/fa";
+import { FaRegCheckCircle } from "react-icons/fa";
+import { MdOutlineDocumentScanner } from "react-icons/md";
 
 function StatusBantuan() {
-
   return (
-    <div className='contain'>
-    
-            <div className="back">
-                <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14.8625 3.06385L13.3792 1.6626L5.13751 9.5001L13.3875 17.3376L14.8625 15.9363L8.08751 9.5001L14.8625 3.06385Z" fill="#8692A6"/>
-                </svg>
-                <a href="#">Kembali</a>
-            </div>
-            <div className="isi">
-                <div className="tentang-bantuan">
-                    <div className="ttg">
-                        <img src={nus} alt="" />
-                        <p>Bantuan Pendidikan </p>
-                        
-                    </div>
-                    <p>Bantuan Pendidikan <br/>Jenjang S1 Membe....</p>  
-                </div>  
-                <div className="status-bantuan">
-                        <div className="heading">
-                            <img src={nus} alt="" />
-                            <h1>Bantuan Pendidikan <br/>
-                            Jenjang S1</h1>
-                        </div>
-                        <div className="stats">
-                          <div className="status">
-                            <img src={ds} alt="" />
-                            <p>Lengkapi<br/>Dokumen</p>
-                          </div>
-                          <img src={ld} alt="" id='i'/>
-                          <div className="status">
-                              <img src={lx} alt="" />
-                              <p>Verifikasi<br/>Dokumen</p>
-                          </div>
-                          <img src={ld} alt="" id='i' />
-                          <div className="status">
-                            <img src={dc} alt="" />
-                            <p>Pengajuan<br/>Berhasil</p>
-                          </div>
-                        </div>
+    <Card className='SB-Container'>
+      <Link to='/' className=' i-SB'>
+        <FaChevronLeft />
+        Kembali
+      </Link>
 
-                        <div className="abt">
-                          <p>Lengkapi Dokuemn Anda Terlebih Dahulu</p>
-                        </div>
-                        <div className="abt">
-                          <h4>Bantuan Yang diberikan :</h4>
-                          <ul>
-                            <li>SPP dan Uang Gedung Gratis Selama Perkuliahan</li>
-                            <li>Bantuan Uang Saku Rp 4.000.000/Semester</li>
-                            <li>Bantuan Dana Buku Sebesar Rp 500.00</li>
-                          </ul>
-                          <h4>Dokumen Yang Dibutuhkan</h4>
-                          <p>KTP, Rapor SMA,TOEFL,Surat Keterangan Sehat,SKTM</p>
-                        </div>
-                </div>
-                      
-            </div>   
-        </div>
-    
-  )
+      <Card className='SB-Box1'>
+        <Card.Body className='SB-Box-Wrap'>
+          <Card.Img src={nus} alt='Mitra-logo' />
+          <Card.Title className='Box1-Title'>Bantuan Pendidikan </Card.Title>
+        </Card.Body>
+        <Card.Body className='Wrap-Text'>
+          <Card.Text ClassName='Box1-Context'>
+            Bantuan Pendidikan <br />
+            Jenjang S1
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Card className='SB-Box2'>
+        <Card.Body className='Box2-Heading'>
+          <Card.Img src={nus} alt='' />
+          <Card.Title className='Box2-Title'>
+            Bantuan Pendidikan <br />
+            Jenjang S1
+          </Card.Title>
+        </Card.Body>
+
+        <Card.Body className='Box3-Status'>
+          <Card.Text>
+            <FaRegFileAlt className='SB-icons' />
+            <Card.Text className='Status-text'>
+              Melengkapi <br />
+              Dokumen
+            </Card.Text>
+          </Card.Text>
+
+          <Card.Text className='SB-Line'></Card.Text>
+          <Card.Text>
+            <MdOutlineDocumentScanner className='SB-icons' />
+            <Card.Text className='Status-text'>
+              Verifikasi <br />
+              Dokumen
+            </Card.Text>
+          </Card.Text>
+
+          <Card.Text className='SB-Line'></Card.Text>
+          <Card.Text>
+            <FaRegCheckCircle className='SB-icons' />
+            <Card.Text className='Status-text'>
+              Pengajuan <br />
+              Berhasil
+            </Card.Text>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Card className='Box4'>
+        <Card.Text className='Box4-Title'>
+          Lengkapi Dokumen Anda Terlebih Dahulu
+        </Card.Text>
+      </Card>
+
+      <Card className='Box5'>
+        <Card.Title className='Box5-Title'>Bantuan Yang diberikan :</Card.Title>
+        <ul>
+          <li>SPP dan Uang Gedung Gratis Selama Perkuliahan</li>
+          <li>Bantuan Uang Saku Rp 4.000.000/Semester</li>
+          <li>Bantuan Dana Buku Sebesar Rp 500.00</li>
+        </ul>
+        <Card.Subtitle className='Box5-Sub'>
+          Dokumen Yang Dibutuhkan
+        </Card.Subtitle>
+        <Card.Text className='Box5-Text'>
+          KTP, Rapor SMA,TOEFL,Surat Keterangan Sehat,SKTM
+        </Card.Text>
+      </Card>
+    </Card>
+  );
 }
 
 export default StatusBantuan;
